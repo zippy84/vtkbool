bbf45f0df48b01a704d6d791d2528f5161ff7048
Ronald Römer <rroemer@gmail.com>
2018-07-06 18:15:16 +0200

    First basic changes

--- libs/vp/RmTrivials.cxx
+++ libs/vp/RmTrivials.cxx
@@ -28,218 +28,6 @@ limitations under the License.
 
 #include "RmTrivials.h"
 
-void MarkInternals (VertsType4 &verts, int skip) {
-    int num = verts.size();
-
-    std::set<int> ids;
-
-    for (int i = 0; i < num; i++) {
-        int j = (i+1)%num,
-            k = (i+2)%num;
-
-        Vert4 &pA = verts[i],
-            &pB = verts[j],
-            &pC = verts[k];
-
-        if (j != skip
-            && !IsNear(pA.pt, pC.pt)
-            && Ld(pA.pt, pB.pt, pC.pt) < 1e-3) {
-
-            pB.marked = std::make_shared<Marked>(i, k);
-
-            ids.insert(j);
-        }
-    }
-
-    std::set<int> invalids;
-
-    for (int id : ids) {
-        for (int i = 0; i < num; i++) {
-            if (ids.count(i) == 0
-                && IsNear(verts[id].pt, verts[i].pt)) {
-                invalids.insert(id);
-            }
-        }
-    }
-
-    for (int id : invalids) {
-        ids.erase(id);
-        verts[id].marked.reset();
-    }
-
-    std::map<Pair, IdsType> col;
-
-    for (int id : ids) {
-        auto &m = verts[id].marked;
-
-        while (ids.count(m->a) == 1) {
-            m->a = (verts[m->a].marked)->a;
-        }
-
-        while (ids.count(m->b) == 1) {
-            m->b = (verts[m->b].marked)->b;
-        }
-
-        Vert4 &pA = verts[m->a],
-            &pB = verts[m->b];
-
-        double v[] = {pB.x-pA.x, pB.y-pA.y},
-            w[] = {verts[id].x-pA.x, verts[id].y-pA.y};
-
-        double v_ = Normalize(v),
-            w_ = Normalize(w);
-
-        m->t = w_/v_;
-
-        col[{m->a, m->b}].push_back(id);
-
-    }
-
-    if (col.size() > 1) {
-        std::map<Pair, IdsType>::iterator itr;
-
-        for (itr = col.begin(); itr != col.end(); ++itr) {
-            const Pair &edge = itr->first;
-            IdsType nodes(itr->second);
-
-            if (nodes.size() > 1) {
-
-                std::sort(nodes.begin(), nodes.end(), [&verts](const int &a, const int &b) {
-                    return (verts[a].marked)->t < (verts[b].marked)->t;
-                });
-
-                nodes.insert(nodes.begin(), edge.f);
-                nodes.push_back(edge.g);
-
-                IdsType keep = {0, static_cast<int>(nodes.size())-1};
-
-                int e;
-
-                do {
-                    e = 0;
-
-                    int l = keep.size();
-                    for (int i = 0; i < l-1; i++) {
-                        int j = i+1;
-
-                        int a = keep[i],
-                            b = keep[j];
-
-                        if (b-a > 1) {
-                            int c = a+(b-a)/2;
-
-                            Vert4 &pA = verts[nodes[a]],
-                                &pB = verts[nodes[b]],
-                                &pC = verts[nodes[c]];
-
-                            if (!(Ld(pA.pt, pB.pt, pC.pt) < 1e-3)) {
-                                e++;
-
-                                keep.insert(keep.begin()+j, c);
-
-                                break;
-                            }
-
-                        }
-                    }
-
-                } while (e > 0);
-
-                if (keep.size() > 2) {
-                    IdsType::iterator itr2;
-
-                    for (itr2 = keep.begin()+1; itr2 != keep.end()-1; ++itr2) {
-                        std::cout << "Rm " << *itr2 << std::endl;
-                        verts[nodes[*itr2]].marked.reset();
-                    }
-
-                }
-
-            }
-        }
-    }
-
-    for (auto& vert : verts) {
-        if (vert.marked) {
-            std::cout << *(vert.marked) << std::endl;
-        }
-    }
-}
-
-void RemoveInternals (VertsType4 &verts) {
-    verts.erase(std::remove_if(verts.begin(), verts.end(), [](const Vert4 &vert) {
-        return vert.marked;
-    }), verts.end());
-
-}
-
-void AlignPts (VertsType4 &verts, int ind) {
-    int num = verts.size();
-
-    double x[] = {verts[ind].x, verts[ind].y};
-
-    VertsType2 verts2;
-
-    for (int i = 0; i < num; i++) {
-        if (i != ind
-            && !verts[i].marked
-            && !IsNear(verts[i].pt, x)) {
-
-            double r[] = {verts[i].pt[0]-x[0], verts[i].pt[1]-x[1]},
-                d = Normalize(r);
-
-            verts2.push_back(Vert2(i, r, d));
-        }
-    }
-
-    std::sort(verts2.begin(), verts2.end());
-
-    /*
-    for (auto& v : verts2) {
-        std::cout << v << std::endl;
-    }
-    */
-
-    for (auto& v : verts2) {
-        double _v[] = {-v.r[1], v.r[0]},
-            _d = _v[0]*x[0]+_v[1]*x[1];
-
-        for (auto& w : verts2) {
-            if (&v == &w) {
-                continue;
-            }
-
-            double d = -(verts[w.i].x*_v[0]+verts[w.i].y*_v[1]-_d);
-
-            double _l = Ld(x, verts[v.i].pt, verts[w.i].pt);
-
-            //std::cout << verts[w.i].id << " -> " << _l << std::endl;
-
-            if (_l < 1e-3) {
-                verts[w.i].pt[0] += d*_v[0];
-                verts[w.i].pt[1] += d*_v[1];
-            }
-        }
-    }
-
-    // korrektur der internen
-
-    for (auto& vert : verts) {
-        auto &m = vert.marked;
-
-        if (m) {
-            const Vert4 &pA = verts[m->a],
-                &pB = verts[m->b];
-
-            double v[] = {pB.x-pA.x, pB.y-pA.y};
-
-            vert.pt[0] = pA.x+m->t*v[0];
-            vert.pt[1] = pA.y+m->t*v[1];
-        }
-    }
-
-}
-
 void TrivialRm::RemovePockets (VertsType3 &good, double *rot, double d, Src src) {
 
     std::vector<Pair2> pairs;
@@ -604,35 +392,27 @@ bool TrivialRm::HasArea (const IdsType &pocket) {
 }
 
 void TrivialRm::GetSimplified (PolyType &res) {
-    VertsType4 poly_(poly.begin(), poly.end());
+    VertsType4 poly2(poly.begin(), poly.end());
 
-    MarkInternals(poly_, ind);
-    AlignPts(poly_, ind);
+    int ind2 = (std::find_if(poly2.begin(), poly2.end(), [&](const Point &p) { return p.id == ind; }))-poly2.begin();
 
-    poly.clear();
-    poly.insert(poly.end(), poly_.begin(), poly_.end());
+    int numPts = poly2.size();
 
-    RemoveInternals(poly_);
+    int iA = (ind2+1)%numPts,
+        iB = (ind2+numPts-1)%numPts;
 
-    int ind_ = (std::find_if(poly_.begin(), poly_.end(), [&](const Point &p) { return p.id == ind; }))-poly_.begin();
-
-    int numPts = poly_.size();
-
-    int iA = (ind_+1)%numPts,
-        iB = (ind_+numPts-1)%numPts;
-
-    double rA[] = {poly_[iA].x-poly_[ind_].x, poly_[iA].y-poly_[ind_].y},
-        rB[] = {poly_[iB].x-poly_[ind_].x, poly_[iB].y-poly_[ind_].y};
+    double rA[] = {poly2[iA].x-poly2[ind2].x, poly2[iA].y-poly2[ind2].y},
+        rB[] = {poly2[iB].x-poly2[ind2].x, poly2[iB].y-poly2[ind2].y};
 
     int num = numPts;
 
     std::map<int, std::vector<Src>> srcs;
 
     for (int i = 0; i < numPts; i++) {
-        verts.push_back(poly_[i]);
+        verts.push_back(poly2[i]);
         Vert3 &last = verts.back();
 
-        if (i == ind_) {
+        if (i == ind2) {
             continue;
         }
 
@@ -642,8 +422,8 @@ void TrivialRm::GetSimplified (PolyType &res) {
 
         double t;
 
-        if (Ld(poly_[ind_].pt, poly_[iA].pt, poly_[i].pt) < 1e-3) {
-            t = GetT(poly_[ind_].pt, poly_[iA].pt, poly_[i].pt);
+        if (Ld(poly2[ind2].pt, poly2[iA].pt, poly2[i].pt)) {
+            t = GetT(poly2[ind2].pt, poly2[iA].pt, poly2[i].pt);
 
             if (t > 1-E) {
                 last.src = Src::A;
@@ -653,7 +433,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
             }
 
         } else {
-            std::shared_ptr<D> d(Intersect(poly_[ind_].pt, rA, poly_[i].pt, poly_[j].pt));
+            std::shared_ptr<D> d(Intersect(poly2[ind2].pt, rA, poly2[i].pt, poly2[j].pt));
 
             if (d && d->t1 > E) {
                 newVerts.emplace(d->t2, Vert3(d->s, Src::A, d->t1));
@@ -661,8 +441,8 @@ void TrivialRm::GetSimplified (PolyType &res) {
             }
         }
 
-        if (Ld(poly_[ind_].pt, poly_[iB].pt, poly_[i].pt) < 1e-3) {
-            t = GetT(poly_[ind_].pt, poly_[iB].pt, poly_[i].pt);
+        if (Ld(poly2[ind2].pt, poly2[iB].pt, poly2[i].pt)) {
+            t = GetT(poly2[ind2].pt, poly2[iB].pt, poly2[i].pt);
 
             if (t > 1-E) {
                 last.src = Src::B;
@@ -672,7 +452,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
             }
 
         } else {
-            std::shared_ptr<D> d(Intersect(poly_[ind_].pt, rB, poly_[i].pt, poly_[j].pt));
+            std::shared_ptr<D> d(Intersect(poly2[ind2].pt, rB, poly2[i].pt, poly2[j].pt));
 
             if (d && d->t1 > E) {
                 newVerts.emplace(d->t2, Vert3(d->s, Src::B, d->t1));
@@ -695,7 +475,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
         return p.id == ind;
     });
 
-    ind_ = nxt-verts.begin();
+    ind2 = nxt-verts.begin();
 
     assert(nxt != verts.end());
 
@@ -715,7 +495,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
         for (int i = 0; i < num; i++) {
             int j = (i+1)%num;
 
-            std::shared_ptr<D> d(Intersect(verts[ind_].pt, _rA, verts[i].pt, verts[j].pt));
+            std::shared_ptr<D> d(Intersect(verts[ind2].pt, _rA, verts[i].pt, verts[j].pt));
 
             if (d && d->t1 > E) {
                 bnd = i;
@@ -725,7 +505,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
         }
 
         for (int i = 0; i < num; i++) {
-            int j = (ind_+i)%num;
+            int j = (ind2+i)%num;
 
             if (verts[j].src != Src::NONE) {
                 verts[j].src = Src::A;
@@ -744,7 +524,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
                 verts[j].rm = true;
             }
 
-            if (j == ind_) {
+            if (j == ind2) {
                 break;
             }
         }
@@ -773,7 +553,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
         });
 
         auto first = std::find_if(good.begin(), good.end(), [&](const Vert3 &p) {
-            return p.id == poly_[iA].id;
+            return p.id == poly2[iA].id;
         });
 
         std::rotate(good.begin(), first, good.end());
@@ -809,7 +589,7 @@ void TrivialRm::GetSimplified (PolyType &res) {
         });
 
         auto first = std::find_if(good.begin(), good.end(), [&](const Vert3 &p) {
-            return p.id == poly_[iB].id;
+            return p.id == poly2[iB].id;
         });
 
         std::rotate(good.begin(), first, good.end());
@@ -843,131 +623,8 @@ void TrivialRm::GetSimplified (PolyType &res) {
         return ind == p.id;
     }), verts.end());
 
-    VertsType4 verts_(verts.begin(), verts.end());
-
-    MarkInternals(verts_, 0);
-    RemoveInternals(verts_);
-
-    std::transform(verts_.begin(), verts_.end(), std::back_inserter(res), [](Vert4 &p) {
-        return p;
-    });
+    std::copy(verts.begin(), verts.end(), std::back_inserter(res));
 
     assert(res.front().id == ind);
-
-}
-
-void AddInternals (PolyType &origin, PolyType &poly) {
-    PolyType res;
-
-    int numA = poly.size(),
-        numB = origin.size();
-
-    for (int i = 0; i < numA; i++) {
-        int j = (i+1)%numA;
-
-        Point &pA = poly[i],
-            &pB = poly[j];
-
-        double u[] = {pB.x-pA.x, pB.y-pA.y},
-            r = Normalize(u);
-
-        res.push_back(pA);
-
-        if (IsNear(pA.pt, pB.pt)) {
-            // spezialfall (special:0, ind:13)
-
-            assert(pA.id != NO_USE && pB.id != NO_USE);
-
-            int k = pB.id;
-            while (k != pA.id) {
-                k = (k+1)%numB;
-
-                res.push_back(origin[k]);
-            }
-
-            res.pop_back();
-
-            continue;
-        }
-
-        VertsType5 verts;
-
-        for (auto& p : origin) {
-            if (IsOnSeg(pA.pt, pB.pt, p.pt)) {
-                double v[] = {p.x-pA.x, p.y-pA.y},
-                    t = Normalize(v)/r;
-                verts.push_back({p, t});
-            }
-        }
-
-        std::sort(verts.begin(), verts.end());
-
-        /*
-        for (auto& v : verts) {
-            std::cout << v << std::endl;
-        }
-        */
-
-        VertsType5::iterator itr;
-
-        if (verts.size() > 1) {
-            std::map<int, double> all;
-
-            all[pA.id] = 0;
-            all[pB.id] = 1;
-
-            for (auto& v : verts) {
-                all[v.id] = v.t;
-            }
-
-            for (itr = verts.begin(); itr != verts.end()-1; ++itr) {
-                if (((itr+1)->t-itr->t) < E) {
-                    int idA = itr->id,
-                        idB = (itr+1)->id;
-
-                    int cA, cB;
-
-                    {
-                        int a = (idA+1)%numB,
-                            b = (idA+numB-1)%numB;
-
-                        //cA = all.count(a)+all.count(b);
-                        cA = (all.count(a) == 1 && all[a] < all[idA])
-                            +(all.count(b) == 1 && all[b] > all[idA]);
-                    }
-
-                    {
-                        int a = (idB+1)%numB,
-                            b = (idB+numB-1)%numB;
-
-                        //cB = all.count(a)+all.count(b);
-                        cB = (all.count(a) == 1 && all[a] < all[idB])
-                            +(all.count(b) == 1 && all[b] > all[idB]);
-                    }
-
-                    std::cout << "(" << idA << "->" << cA << ", "
-                        << idB << "->" << cB << ") => " << poly.front().id << std::endl;
-
-                    assert(cA > 0 || cB > 0);
-
-                    if (cA > cB) {
-                        (itr+1)->valid = false;
-                    } else {
-                        itr->valid = false;
-                    }
-
-                }
-            }
-        }
-
-        for (auto& v : verts) {
-            if (v.valid) {
-                res.push_back(v);
-            }
-        }
-
-    }
-
-    poly.swap(res);
 
 }
