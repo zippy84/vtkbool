df17de50929c8eb6ecaf9661e119a3045d0803eb
hwang822 <hwang822@gmail.com>
2019-08-20 10:56:56 -0500

    fixed bug for vtkboo.cmake.in and vtkbooConfig.cmake

---
+++ out/build/x64-Debug/CMakeFiles/vtkboolConfig.cmake
@@ -0,0 +1,11 @@
+# - Config file for the VTKBOOL package
+# It defines the following variables
+#  VTKBOOL_INCLUDE_DIRS - include directories for MeshFix
+#  VTKBOOL_LIBRARIES    - libraries to link against
+
+# Compute paths
+get_filename_component(VTKBOOL_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(VTKBOOL_INCLUDE_DIRS "${VTKBOOL_CMAKE_DIR}/../include")
+include("${VTKBOOL_CMAKE_DIR}/VTKBoolTargets.cmake")
+set(VTKBOOL_LIBRARIES VTKBOOL)
+
---
+++ out_good/build/x64-Debug/libs/decomp_build/cmake_install.cmake
@@ -0,0 +1,56 @@
+# Install script for directory: C:/Workarea/vtkbool/libs/decomp
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Workarea/vtkbool/out/install/x64-Debug")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Workarea/vtkbool/out/build/x64-Debug/libs/decomp_build/decomp.lib")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xdevx" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/vtkbool/libs/decomp/Decomposer.h")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/vtkbool/libs/decomp" TYPE FILE FILES "C:/Workarea/vtkbool/libs/decomp/Decomposer.h")
+endif()
+
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for each subdirectory.
+  include("C:/Workarea/vtkbool/out/build/x64-Debug/libs/decomp_build/vp_build/cmake_install.cmake")
+
+endif()
+
