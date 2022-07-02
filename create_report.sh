1524141440427838a334b103271beb38f52c28b7
Ronald Römer <rroemer@gmail.com>
2022-07-02 19:23:39 +0200

    better coverage 2

---
+++ create_report.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+[ ! -d coverage_report ] && mkdir coverage_report
+docker build -t vtkbool_coverage -f Dockerfile.archlinux .
+id=$(docker run -t -d vtkbool_coverage)
+docker exec $id sh -c 'tar -cf - /refactoring/build/coverage.*' | tar --strip-components=2 -xf - -C coverage_report
+docker stop -t 0 $id
