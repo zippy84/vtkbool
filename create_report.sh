3bef20aad83e1d3ba7b33b6e883ee87a35146708
Ronald Römer <rroemer@gmail.com>
2023-05-09 23:08:51 +0200

    cleanup

--- create_report.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ ! -d coverage_report ] && mkdir coverage_report
-docker build -t vtkbool_coverage -f Dockerfile.archlinux .
-id=$(docker run -t -d vtkbool_coverage)
-docker exec $id sh -c 'tar -cf - /master/build/coverage.*' | tar --strip-components=2 -xf - -C coverage_report
-docker stop -t 0 $id
